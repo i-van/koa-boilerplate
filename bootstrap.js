4e51cea4bff86829a507411beea9f3e9b1281cf7
Ivan Nosov <i.k.nosov@gmail.com>
2016-09-26 17:20:22 +0300

    heroku setup

--- bootstrap.js
+++ bootstrap.js
@@ -2,6 +2,6 @@
 require('dotenv').config({ silent: true });
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 require('./lib/validation');
