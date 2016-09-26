7084fa24205afff701989dd964aeeba11382718c
Ivan Nosov <i.k.nosov@gmail.com>
2016-09-26 10:49:18 +0300

    cleanup

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const app = module.exports = require('koa')();
 
-require('dotenv').config();
+require('./bootstrap');
+
+const app = module.exports = require('koa')();
+const jwt = require('koa-jwt');
 
 app.use(require('koa-logger')());
 app.use(require('koa-bodyparser')());
 app.use(require('koa-cors')());
 require('koa-validate')(app);
 
-app.use(require('./lib/response')());
-app.use(require('./lib/mongo')(app));
-app.use(require('./lib/jwt')(app));
-app.use(require('./lib/acl')(app));
+app.use(require('./lib/response'));
+require('./lib/acl')(app);
 app.use(require('./lib/validation')());
-app.use(require('./lib/router')(app));
+
+app.use(jwt({ secret: process.env.AUTH_JWT_SECRET }).unless({ path: ['/auth/sign-in', '/auth/sign-up'] }));
+require('./app/routes')(app);
 
 if (!module.parent) {
   app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
