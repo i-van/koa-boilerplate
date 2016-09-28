39ff94b3a4f30e3594aa63f1a1ff29669ad57644
Ivan Nosov <i.k.nosov@gmail.com>
2016-09-28 18:03:47 +0300

    cleanup

--- test/app/controllers/reports.js
+++ test/app/controllers/reports.js
@@ -16,30 +16,28 @@ describe('app/controllers/user', function() {
   const token = jwt.sign(user, process.env.AUTH_JWT_SECRET, { expiresInMinutes: 60 * 5 });
 
   describe('list', function() {
-    const report = {
-      date: '2016-05-05',
-      timeTaken: 3600,
-      description: 'It takes 1 hour'
-    };
-
-    before(function() {
-      return Report.create(report);
+    before(function*() {
+      this.report = yield Report.create({
+        date: '2016-05-05',
+        timeTaken: 3600,
+        description: 'It takes 1 hour'
+      });
     });
 
     after(function() {
-      return Report.remove();
+      return this.report.remove();
     });
 
     it('returns reports', function(done) {
       request
         .get('/reports')
         .set('Authorization', 'Bearer ' + token)
-        .expect(function(res) {
+        .expect(res => {
           expect(res.body).to.have.length(1);
-          expect(res.body[0].id).to.exist;
-          expect(res.body[0].date).to.exist;
-          expect(res.body[0].timeTaken).to.equal(report.timeTaken);
-          expect(res.body[0].description).to.equal(report.description);
+          expect(res.body[0].id).to.equal(this.report._id.toString());
+          expect(res.body[0].date).to.equal(this.report.date.toISOString());
+          expect(res.body[0].timeTaken).to.equal(this.report.timeTaken);
+          expect(res.body[0].description).to.equal(this.report.description);
         })
         .expect(200)
         .end(done);
@@ -47,31 +45,27 @@ describe('app/controllers/user', function() {
   });
 
   describe('show', function() {
-    let report;
-
-    before(function() {
-      return Report
-        .create({
-          date: '2016-05-05',
-          timeTaken: 3600,
-          description: 'It takes 1 hour'
-        })
-        .then(model => report = model);
+    before(function*() {
+      this.report = yield Report.create({
+        date: '2016-05-05',
+        timeTaken: 3600,
+        description: 'It takes 1 hour'
+      });
     });
 
     after(function() {
-      return Report.remove();
+      return this.report.remove();
     });
 
     it('returns report', function(done) {
       request
-        .get('/reports/' + report._id)
+        .get('/reports/' + this.report._id)
         .set('Authorization', 'Bearer ' + token)
-        .expect(function(res) {
-          expect(res.body.id).to.equal(report._id.toString());
-          expect(res.body.date).to.equal(report.date.toISOString());
-          expect(res.body.timeTaken).to.equal(report.timeTaken);
-          expect(res.body.description).to.equal(report.description);
+        .expect(res => {
+          expect(res.body.id).to.equal(this.report._id.toString());
+          expect(res.body.date).to.equal(this.report.date.toISOString());
+          expect(res.body.timeTaken).to.equal(this.report.timeTaken);
+          expect(res.body.description).to.equal(this.report.description);
         })
         .expect(200)
         .end(done);
@@ -79,35 +73,31 @@ describe('app/controllers/user', function() {
   });
 
   describe('update', function() {
-    let report;
-
-    before(function() {
-      return Report
-        .create({
-          date: '2016-05-05',
-          timeTaken: 3600,
-          description: 'It takes 1 hour'
-        })
-        .then(model => report = model);
+    before(function*() {
+      this.report = yield Report.create({
+        date: '2016-05-05',
+        timeTaken: 3600,
+        description: 'It takes 1 hour'
+      });
     });
 
     after(function() {
-      return Report.remove();
+      return this.report.remove();
     });
 
     it('updates report', function(done) {
       request
-        .put('/reports/' + report._id)
+        .put('/reports/' + this.report._id)
         .set('Authorization', 'Bearer ' + token)
         .send({
-          date: report.date,
-          timeTaken: report.timeTaken,
+          date: this.report.date,
+          timeTaken: this.report.timeTaken,
           description: 'new description'
         })
-        .expect(function(res) {
-          expect(res.body.id).to.equal(report._id.toString());
-          expect(res.body.date).to.equal(report.date.toISOString());
-          expect(res.body.timeTaken).to.equal(report.timeTaken);
+        .expect(res => {
+          expect(res.body.id).to.equal(this.report._id.toString());
+          expect(res.body.date).to.equal(this.report.date.toISOString());
+          expect(res.body.timeTaken).to.equal(this.report.timeTaken);
           expect(res.body.description).to.equal('new description');
         })
         .expect(200)
@@ -116,24 +106,24 @@ describe('app/controllers/user', function() {
   });
 
   describe('destroy', function() {
-    let report;
-
-    before(function() {
-      return Report
-        .create({
-          date: '2016-05-05',
-          timeTaken: 3600,
-          description: 'It takes 1 hour'
-        })
-        .then(model => report = model);
+    before(function*() {
+      this.report = yield Report.create({
+        date: '2016-05-05',
+        timeTaken: 3600,
+        description: 'It takes 1 hour'
+      });
     });
 
     it('removes report', function(done) {
       request
-        .delete('/reports/' + report._id)
+        .delete('/reports/' + this.report._id)
         .set('Authorization', 'Bearer ' + token)
         .expect(204)
         .end(done);
+    });
+
+    it('count of reports should be 0', function*() {
+      expect(yield Report.count()).to.equal(0);
     });
   });
 });
